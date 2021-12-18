072f366df3ecf1860f08153e757515387c9d912f
steven cardona <you@example.com>
2021-12-17 23:34:34 -0500

    Base files  contain the components of our project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AppRouter } from "./Router/Main";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Provider } from "./Context/Provider";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Provider>
+				<AppRouter />
+				{/* Reset All Styles of Browser */}
+				<CssBaseline />
+			</Provider>
+		</div>
+	);
 }
 
 export default App;
